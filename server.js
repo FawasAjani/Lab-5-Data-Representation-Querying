ce13f3d895605b22cdbce535584c20e8eec85901
Fawas Ajani <G0041322@atu.ie>
2024-10-25 15:42:13 +0000

    update

--- server.js
+++ server.js
@@ -24,3 +24,31 @@ app.get('/hello/:fname', (req, res) => {
 });
 
 //Exercise 5: Return JsonData
+
+//set up movies page
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    res.status(201).json({ myMovies:movies });
+});
